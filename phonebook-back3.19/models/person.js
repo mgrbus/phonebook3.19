4f1555a44e7c4f2344280514a0b319bb21b29f64
matija grbus <matijagrbus@yahoo.com>
2024-09-18 07:04:45 +0200

    uibuild

--- phonebook-back3.19/models/person.js
+++ phonebook-back3.19/models/person.js
@@ -1,3 +1,4 @@
+
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
@@ -19,7 +20,16 @@ const personSchema = new mongoose.Schema({
         minLength: 3
     },
     number: {
-        type:Number},
+        type:String,
+        minLength:8,
+        validate: {
+            validator: function(v) {
+                return /\d{2,3}-\d{6,10}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number`
+        },
+        required: [true, 'User phone number required!']
+        },
     important: Boolean
 })
 
